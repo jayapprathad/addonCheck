0658d00fd6a78e82216c03510a4f121777abb93d
jayappratha <jayapprathad@gmail.com>
2023-07-11 17:21:06 +0530

    component

---
+++ addon/components/btn.js
@@ -0,0 +1,6 @@
+import Component from '@ember/component';
+import layout from '../templates/components/btn';
+
+export default Component.extend({
+  layout
+});
---
+++ app/components/btn.js
@@ -0,0 +1,1 @@
+export { default } from 'addon-check/components/btn';
